4bd9e55cdd592a19e9ba1af1b9730704c48bd750
danilbushkov <danil1357997@gmail.com>
2023-04-15 20:02:22 +0300

    add drag and drop for test element

--- editor/src/App.vue
+++ editor/src/App.vue
@@ -1,104 +1,16 @@
 <template>
-  <div 
-    id="editor"
-    :style="editorStyle"
-    @mouseup="onMouseUp"
-    @mousemove="onMouseMove">
-    
-    <div 
-      :style="styleObject" 
-      @mousedown="onMouseDown($event)"
-      @dragstart="onDragStart"
-      ref="testt"
-      id="test">
-
-    </div>
-  </div>
+  <div>Hello</div>
 </template>
 
 <script>
 
-import { ref } from 'vue'
 
 export default {
-  data() {
-    return {
-      mouseDown: false,
-      left: 0,
-      top: 0,
-      editorLeft: 0,
-      editorTop: 0,
-      shiftX: 0,
-      shiftY: 0,
-      //focus: false,
-    }
-  },
-  computed: {
-    styleObject() {
-      return {
-        left: this.left + "px",
-        top: this.top + "px",
-      }
-    },
-    editorStyle() {
-      return {
-        left: this.editorLeft + "px",
-        top: this.editorTop + "px",
-      }
-    }
-  },
-  methods: {
-    onMouseDown(event) {
-      this.mouseDown = true;
-      const rect = this.testt.getBoundingClientRect();
-      this.shiftX = event.clientX - rect.left;
-      this.shiftY = event.clientY - rect.top;
-      //this.testt.addEventListener('mousemove', this.onMouseMove);
-      //this.testt.addEventListener('mouseup', this.onMouseUp);
-      //this.focus = this.focus;
-    },
-    onMouseUp() {
-      
-      this.mouseDown = false;
-    },
-    onMouseMove(event) {
-      if(this.mouseDown) {
-        this.left = event.pageX - this.shiftX;
-        this.top = event.pageY - this.shiftY;
-      }
-      
-    },
-    onDragStart() {
-      return false;
-    }
-  },
-  setup() {
-    const testt = ref(null)
-    return {
-      testt
-    }
-  }
-  
+  name: 'App',
   
 }
 </script>
 
 <style>
-  #editor {
-    position: relative;
-    margin: 0;
-    padding: 0;
-    overflow-y: scroll;
-    overflow-block: scroll;
-    background-color: red;
-    width: 800px;
-    height: 800px;
-  }
-  #test {
-    width: 100px;
-    height: 100px;
-    background-color: aqua;
-    position: absolute;
-    z-index: 1;
-  }
+
 </style>
