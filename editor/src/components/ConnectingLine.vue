c6f3f51fb43f5d6f735a84faa522c2355d40a8e0
danilbushkov <danil1357997@gmail.com>
2023-05-04 22:27:42 +0300

    add connecting line

---
+++ editor/src/components/ConnectingLine.vue
@@ -0,0 +1,107 @@
+<template>
+  <svg 
+    class="conn-line"
+    :style="lineStyleObject" 
+     xmlns="http://www.w3.org/2000/svg">
+    <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
+  </svg>  
+</template>
+
+
+<script>
+
+
+export default {
+  props: {
+    absoluteX1: {
+      type: Number,
+      default: 0,
+    },
+    absoluteY1: {
+      type: Number,
+      default: 0,
+    },
+    absoluteX2: {
+      type: Number,
+      default: 0,
+    },
+    absoluteY2: {
+      type: Number,
+      default: 0,
+    },
+  },
+  
+  data() {
+
+  },
+  computed: {
+    lineStyleObject() {
+      
+      let top = 0;
+      let left = 0;
+
+      const h = this.absoluteY1 - this.absoluteY2;
+      const w = this.absoluteX2 - this.absoluteX1;
+      if(h >= 0) {
+        top = this.absoluteY2;
+      } else {
+        top = this.absoluteY1;
+      }
+      if(w >= 0) {
+        left = this.absoluteX1;
+      } else {
+        left = this.absoluteX2;
+      }
+      
+      return {
+        left: left+"px",
+        top: top+"px",
+        width: Math.abs(w)+"px",
+        height: Math.abs(h)+"px",
+        
+      }
+    },
+    relativeX1() {
+      const w = this.absoluteX2 - this.absoluteX1;
+      if(w >= 0) {
+        return 0;
+      }
+      return w;
+    },
+    relativeX2() {
+      const w = this.absoluteX2 - this.absoluteX1;
+      if(w >= 0) {
+        return w;
+      }
+      return 0;
+    },
+    relativeY1() {
+      const h = this.absoluteY1 - this.absoluteY2;
+      if(h >= 0) {
+        return 0;
+      }
+      return h;
+    },
+    relativeY2() {
+      const h = this.absoluteY1 - this.absoluteY2;
+      if(h >= 0) {
+        return h;
+      }
+      return 0;
+    },
+
+  },
+  
+  setup() {
+    
+  },
+}
+</script>
+
+
+
+<style>
+.conn-line {
+  position: absolute;
+}
+</style>
