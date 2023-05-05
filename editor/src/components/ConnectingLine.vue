ae24c5b674b0136b9dd18632a7d428cac17c1b1a
danilbushkov <danil1357997@gmail.com>
2023-05-05 19:52:36 +0300

    add padding to the line

--- editor/src/components/ConnectingLine.vue
+++ editor/src/components/ConnectingLine.vue
@@ -37,7 +37,7 @@ export default {
   
   data() {
     return {
-
+      padding: 10,
     }
   },
   computed: {
@@ -46,54 +46,56 @@ export default {
       let top = 0;
       let left = 0;
 
-      const h = this.absoluteY2 - this.absoluteY1;
-      const w = this.absoluteX2 - this.absoluteX1;
+      let h = this.absoluteY2 - this.absoluteY1;
+      let w = this.absoluteX2 - this.absoluteX1;
       if(h >= 0) {
-        top = this.absoluteY1;
+        top = this.absoluteY1 - this.padding;
       } else {
-        top = this.absoluteY2;
+        top = this.absoluteY2 - this.padding;
       }
       if(w >= 0) {
-        left = this.absoluteX1;
+        left = this.absoluteX1 - this.padding;
       } else {
-        left = this.absoluteX2;
+        left = this.absoluteX2 - this.padding;
       }
+      h = Math.abs(h) + this.padding*2;
+      w = Math.abs(w) + this.padding*2;
       
       return {
         left: left+"px",
         top: top+"px",
-        width: Math.abs(w)+"px",
-        height: Math.abs(h)+"px",
+        width: w+"px",
+        height: h+"px",
         
       }
     },
     relativeX1() {
       const w = this.absoluteX2 - this.absoluteX1;
       if(w >= 0) {
-        return 0;
+        return this.padding;
       }
-      return Math.abs(w);
+      return Math.abs(w)+this.padding;
     },
     relativeX2() {
       const w = this.absoluteX2 - this.absoluteX1;
       if(w >= 0) {
-        return w;
+        return w+this.padding;
       }
-      return 0;
+      return this.padding;
     },
     relativeY1() {
       const h = this.absoluteY2 - this.absoluteY1;
       if(h >= 0) {
-        return 0;
+        return this.padding;
       }
-      return Math.abs(h);
+      return Math.abs(h)+this.padding;
     },
     relativeY2() {
       const h = this.absoluteY2 - this.absoluteY1;
       if(h >= 0) {
-        return h;
+        return h+this.padding;
       }
-      return 0;
+      return this.padding;
     },
 
   },
