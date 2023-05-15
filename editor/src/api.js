901419d50c7c1e97a248d606f60fdceb4c5617ed
danilbushkov <danil1357997@gmail.com>
2023-05-15 17:24:00 +0300

    add api requests: delete token, start bot, stop bot

--- editor/src/api.js
+++ editor/src/api.js
@@ -1,6 +1,6 @@
 
 
-import { domain, user_token } from './conf.js'
+import { domain, user_token, bot_token } from './conf.js'
 
 const site_url = "https://" + domain;
 const headers = {
@@ -33,3 +33,64 @@ export async function createBot(bot_name) {
     }
     return null;
 }
+
+export async function setBotToken(id) {
+    const url = site_url + "/api/bots/" + id + "/token"
+    const token = {
+        token: bot_token,
+    }
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(token),
+    });
+    if(response.ok) {
+        const json = response.json();
+        if(!json.ok) {
+            console.error("set bot token error: " + json.error.message);
+        } 
+    } else {
+        console.error("set bot token error: " + response.status);
+        
+    }
+    return null;
+}
+
+
+export async function startBot(id) {
+    const url = site_url + "/api/bots/"+ id + "/start"
+    const response = await fetch(url, {
+        method: 'PATCH',
+        headers: headers,
+    });
+    if(response.ok) {
+        const json = response.json();
+        if(!json.ok) {
+            console.error("start bot error: " + json.error.message);
+        } 
+    } else {
+        console.error("start bot error: " + response.status);
+        
+    }
+    return null;
+}
+
+export async function stopBot(id) {
+    const url = site_url + "/api/bots/"+ id + "/stop"
+    const response = await fetch(url, {
+        method: 'PATCH',
+        headers: headers,
+    });
+    if(response.ok) {
+        const json = response.json();
+        if(!json.ok) {
+            console.error("stop bot error: " + json.error.message);
+        } 
+    } else {
+        console.error("stop bot error: " + response.status);
+        
+    }
+    return null;
+}
+
+
