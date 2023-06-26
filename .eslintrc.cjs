cd2e26bfd3b6cf49d33fb2015bb94d148daa6e85
danilbushkov <danil1357997@gmail.com>
2023-06-26 22:05:25 +0300

    add check types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+    
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:solid/typescript",
     "prettier",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
   },
   plugins: ["@typescript-eslint", "solid", "prettier"],
   rules: {
     "prettier/prettier": ["error"],
   },
+  root: true,
 };
