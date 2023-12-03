282af260a3b715c19d50c8287366fd516b818c77
danilbushkov <danil1357997@gmail.com>
2023-12-03 15:38:56 +0300

    delete return error in HTTPClient methods

--- src/api/HTTPClient.ts
+++ src/api/HTTPClient.ts
@@ -1,29 +1,32 @@
+export interface ServiceError {
+  code: number;
+  message: string;
+}
+
 export interface HTTPResponse<T> {
-  data?: T;
   status: number;
+  data?: T;
+  error?: ServiceError;
 }
 
 export class HTTPClient {
   constructor(private _url: string) {}
 
-  async GET<T>(
-    path: string,
-    token?: string
-  ): Promise<[HTTPResponse<T>, Error?]> {
+  async GET<T>(path: string, token?: string): Promise<HTTPResponse<T>> {
     return this.request(path, "GET", token);
   }
   async POST<I, O>(
     path: string,
     data?: I,
     token?: string
-  ): Promise<[HTTPResponse<O>, Error?]> {
+  ): Promise<HTTPResponse<O>> {
     return this.request(path, "POST", token, data);
   }
   async PATCH<I, O>(
     path: string,
     data?: I,
     token?: string
-  ): Promise<[HTTPResponse<O>, Error?]> {
+  ): Promise<HTTPResponse<O>> {
     return this.request(path, "PATCH", token, data);
   }
 
@@ -31,13 +34,10 @@ export class HTTPClient {
     path: string,
     data?: I,
     token?: string
-  ): Promise<[HTTPResponse<O>, Error?]> {
+  ): Promise<HTTPResponse<O>> {
     return this.request(path, "PUT", token, data);
   }
-  async DELETE<T>(
-    path: string,
-    token?: string
-  ): Promise<[HTTPResponse<T>, Error?]> {
+  async DELETE<T>(path: string, token?: string): Promise<HTTPResponse<T>> {
     return this.request(path, "DELETE", token);
   }
 
@@ -46,7 +46,7 @@ export class HTTPClient {
     method: string,
     token?: string,
     data?: I
-  ): Promise<[HTTPResponse<O>, Error?]> {
+  ): Promise<HTTPResponse<O>> {
     let options = {};
     if (data == undefined) {
       if (token != undefined) {
@@ -83,14 +83,16 @@ export class HTTPClient {
     }
     const response = await fetch(this._url + path, options);
     const result = {
-      data: undefined,
       status: response.status,
+      data: undefined,
+      error: undefined,
     };
-    try {
+    if (response.status == 200 || response.status == 201) {
       result.data = await response.json();
-    } catch (e) {
-      return [result, e as Error];
+    } else if (response.status == 422) {
+      result.error = await response.json();
     }
-    return [result, undefined];
+
+    return result;
   }
 }
